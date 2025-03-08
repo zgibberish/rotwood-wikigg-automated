b7a72dba908c68c3f8be930688e6ec89c2a42fb7
gibberish <gbjgms@gmail.com>
2025-03-09 00:50:21 +0700

    (wikiggutil) Updated modinfo to be apiv1 compatible

--- src/wikigg-automated/modinfo.lua
+++ src/wikigg-automated/modinfo.lua
@@ -1,16 +1,35 @@
-name = "Wiki.gg Automated"
-description = "Scripts to help generating wiki.gg by reading content straight from the game."
-author = "gibberish"
-version = "dev"
-api_version = 10
+-- for old apiv10
+-- name = "Wiki.gg Automated"
+-- description = "Scripts to help generating wiki.gg by reading content straight from the game."
+-- author = "gibberish"
+-- version = "dev"
+-- api_version = 10
 
-dst_compatible = false
-forge_compatible = false
-gorge_compatible = false
-dont_starve_compatible = false
-reign_of_giants_compatible = false
-shipwrecked_compatible = false
-rotwood_compatible = true
+-- dst_compatible = false
+-- forge_compatible = false
+-- gorge_compatible = false
+-- dont_starve_compatible = false
+-- reign_of_giants_compatible = false
+-- shipwrecked_compatible = false
+-- rotwood_compatible = true
 
-client_only_mod = true
-all_clients_require_mod = false
+-- client_only_mod = true
+-- all_clients_require_mod = false
+
+-- for apiv1
+return {
+    name = "Wiki.gg Automated",
+    description = "Scripts to help generating wiki.gg by reading content straight from the game.",
+    author = "gibberish",
+    version = "dev",
+    mod_version = "dev",
+    api_version = 1,
+
+    mod_type = "gameplay",
+    supports_mode = {
+		rotwood = true,
+	},
+
+    client_only_mod = true,
+    all_clients_require_mod = false,
+}
