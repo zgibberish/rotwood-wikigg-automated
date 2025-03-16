84f136e3c00d1ebde3ffa58b2ccd06ae8913818c
gibberish <gbjgms@gmail.com>
2025-03-16 22:32:49 +0700

    (scripts) (food icons) Use new filename format for extracted image assets

--- src/scripts/extract_images/food_icons.sh
+++ src/scripts/extract_images/food_icons.sh
@@ -3,5 +3,11 @@ source rwtextool/venv/bin/activate
 unzip data.zip "images/icons_inventory*" 
 find images/icons_inventory*.tex | xargs -I {} python rwtextool/src/tex2img.py -A {}
 find images/*.* | xargs rm # remove tex and atlas files
-find images/*/*food_cooked* | xargs -I {} mv {} images/ # move all pngs out
+for filename in $(find images/*/*food_cooked*); do
+    # move all images out to images/, but
+    # replace slashes in file path to underscores
+    # and use that as the file names
+    new_filename="$(echo "$filename" | tr / _)"
+    mv $filename "images/$new_filename"
+done
 find images/*/ -type d | xargs rm -r # remove remaining folders
