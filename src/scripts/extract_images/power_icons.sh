259795bfdc9f99fbe51259595465f7d466c2ee60
gibberish <gbjgms@gmail.com>
2025-03-17 15:22:05 +0700

    Use new filename format for power icons asset extract script

--- src/scripts/extract_images/power_icons.sh
+++ src/scripts/extract_images/power_icons.sh
@@ -3,5 +3,11 @@ source rwtextool/venv/bin/activate
 unzip data.zip "images/ui_ftf_power_icons*" 
 find images/ui_ftf_power_icons*.tex | xargs -I {} python rwtextool/src/tex2img.py -A {}
 find images/*.* | xargs rm # remove tex and atlas files
-find images/ui_ftf_power_icons*/* | xargs -I {} mv {} images/ # move all pngs out
+for filename in $(find images/ui_ftf_power_icons*/*); do
+    # move all images out to images/, but
+    # replace slashes in file path to underscores
+    # and use that as the file names
+    new_filename="$(echo "$filename" | tr / _)"
+    mv $filename "images/$new_filename"
+done
 find images/*/ -type d | xargs rm -r # remove remaining folders
